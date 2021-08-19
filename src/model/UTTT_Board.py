ee49c94e0abad43f0643297ab1ccef62463d622e
KeeganKresge <KeeganKresge@gmail.com>
2021-08-18 23:33:49 -0400

    Worked on Validating Moves
    Added Is_Valid_Move functions to both TTT and UTTT boards. Note: Does not check to see if next move is made in the correct square
    Added Check_Claimed function to see if the square is claimed after making a move
    Added Check_Players  function to check if three cells have the same player occupying them
    Edited UTTT Make move to check if valid

--- src/model/UTTT_Board.py
+++ src/model/UTTT_Board.py
@@ -1,4 +1,4 @@
-import Observer.py
+# import Observer.py
 import numpy as np
 from enum import Enum
 
@@ -27,12 +27,46 @@ class TTT_Board:
     def __init__(self):
         self.board=np.array([Players.NONE] *9)
         self.status = Claim_Status.NONE
+        self.RowContainer=[0]*3
+        self.ColContainer = [0] * 3
+        self.DiagContainer = 0
+        self.ODiagContainer = 0
+
+    def Is_Valid_Move(self,cell):
+        if self.status==Claim_Status.NONE:
+            return self.board[cell] == Players.NONE
+        else:
+            return False
 
     def Add_Marker(self,Player, cell):
+        #Assuming move is valid, add marker and update containers
         self.board[cell]=Player
+        self.RowContainer[cell // 3]+=1
+        self.ColContainer[cell % 3]+=1
+        if cell%4==0:
+            self.DiagContainer+=1
+        if cell%2==0 and cell%8!=0 :
+            self.ODiagContainer += 1
+        self.Check_Claimed(cell)
 
-    def Is_Claimed(self):
-        return Claim_Status.NONE
+    def Check_Claimed(self,cell):
+        #Updates claimed status after move is made
+        if self.RowContainer[cell//3]==3:
+            self.Check_Players([cell//3, (cell//3) +1, (cell//3)+2])
+        if self.ColContainer[cell%3]==3:
+            self.Check_Players([cell % 3, (cell % 3) + 3, (cell % 3) + 3])
+        if self.DiagContainer==3:
+            self.Check_Players([2,4,6])
+        if self.ODiagContainer==3:
+            self.Check_Players([0,4,8])
+
+    def Check_Players(self,cells):
+        #If the 3 cells given have the same Player, then status is updated to that Player's win
+        if self.status == Claim_Status.NONE: # Added in case multiple Check_Claimed ifs are triggered
+            for i in range(3):
+                if self.board[cells[i]] != self.board[cells[(i+1) %3]]:
+                    return
+            self.status=self.board[cells[0]]
 
     def Get_Cell_Contents(self, cell: int) -> Players:
         return self.board[cell]
@@ -45,10 +79,11 @@ class UTTT_Board:
     Ultimate Tic Tac Toe Board Model
     Has functionality to make moves, check if moves are valid, check game status, get contents of a square, restart game...
     """
-
     def __init__(self):
         #Initate Blank Board
-        self.board=[TTT_Board()] *9
+        self.board=[]
+        [self.board.append(TTT_Board()) for i in range(9)]
+        # self.board=[TTT_Board()] *9
         self.observers=[]
         self.game_status=Game_Status.NOT_OVER
 
@@ -61,10 +96,10 @@ class UTTT_Board:
         for observer in self.observers:
             observer.update(self,string)
 
-    def Make_Move(self, square, cell):
+    def Make_Move(self, square, cell, player):
         # Add marker to the board
-        if(self.Is_Valid_Move(square, cell)):
-            return None
+        if self.Is_Valid_Move(square, cell):
+            self.board[square].board[cell] = player
         else:
             self.Notify_Observers("Invalid Move.")
 
@@ -72,6 +107,5 @@ class UTTT_Board:
         if square<0 or square>8 or cell<0 or cell>9:
             return False
         else:
-            sqr=self.board[square]
-            return sqr.board[cell] ==Players.NONE
+            return self.board[square].Is_Valid_Move(cell)
 
