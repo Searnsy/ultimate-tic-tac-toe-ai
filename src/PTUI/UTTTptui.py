4a50d252c858e2e460d27e2d60449f3c5c8f1af2
KeeganKresge <KeeganKresge@gmail.com>
2022-01-06 19:30:23 -0500

    Moved Human Input into separate function
    This allows us to add other kinds of input for future AI models

--- src/PTUI/UTTTptui.py
+++ src/PTUI/UTTTptui.py
@@ -1,9 +1,9 @@
 from src.model.Observer import *
 from src.model.UTTTBoard import *
 
-
 class UTTTptui(Observer):
     def __init__(self):
+        self.square = -1
         self.model = UTTTBoard()
         self.message = ""
         self.curr_player = Players.X
@@ -22,21 +22,11 @@ class UTTTptui(Observer):
     def run(self):
         print("Welcome to Ultimate Tic Tac Toe!")
         self.update(self.model, None)
-        square = -1
         while self.model.game_status == GameStatus.NOT_OVER:
-            # Continue based off if next square is claimed
-            while True:
-                if 0 <= square < 9 and self.model.board[square].get_claim_status() is ClaimStatus.NONE:
-                    print("Player " + self.curr_player.value + " place your next marker in square " + str(square))
-                else:
-                    square = int(input("Player " + self.curr_player.value + ", enter the square to add your marker: "))
-                cell = int(input("Enter the cell to add your marker: "))
-                self.model.make_move(square, cell, self.curr_player)
-                if self.message != "Invalid Move.":
-                    break
-                else:
-                    print(self.message)
-            square = cell
+            if self.curr_player == Players.X:
+                self.HumanInput()
+            else:
+                self.HumanInput()
             # Switch Player
             if self.curr_player == Players.X:
                 self.curr_player = Players.O
@@ -44,6 +34,24 @@ class UTTTptui(Observer):
                 self.curr_player = Players.X
 
         self.update(self.model, None)
+
+    def HumanInput(self):
+        """
+        Allow Human to input their move
+        """
+        # Continue based off if next square is claimed
+        while True:
+            if 0 <= self.square < 9 and self.model.board[self.square].get_claim_status() is ClaimStatus.NONE:
+                print("Player " + self.curr_player.value + " place your next marker in square " + str(self.square))
+            else:
+                self.square = int(input("Player " + self.curr_player.value + ", enter the square to add your marker: "))
+            cell = int(input("Enter the cell to add your marker: "))
+            self.model.make_move(self.square, cell, self.curr_player)
+            if self.message != "Invalid Move.":
+                break
+            else:
+                print(self.message)
+        self.square = cell
 
 
 if __name__ == '__main__':
